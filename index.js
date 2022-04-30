4a91639ccbec0d53cb54d9f437b6b7ec66096381
Glebova13 <104679900+Glebova13@users.noreply.github.com>
2022-04-30 14:54:47 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ *Submitted for verification at BscScan.com on 2020-09-04
+*/
+
+pragma solidity 0.5.16;
+
+interface IBEP20 {
+  /**
+   * @dev Returns the amount of tokens in existence.
+   */
+  function totalSupply() external view returns (uint256);
+
+  /**
+   * @dev Returns the token decimals.
+   */
+  function decimals() external view returns (uint8);
+
+  /**
+   * @dev Returns the token symbol.
+   */
+  function symbol() external view returns (string memory);
